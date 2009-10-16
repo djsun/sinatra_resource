b8f41587c3d3ee418e7c65b2880249daed4b0e26
David James <djames@sunlightfoundation.com>
2009-10-15 21:19:31 -0400

    Simplify example

--- spec/examples/datacatalog/user.rb
+++ spec/examples/datacatalog/user.rb
@@ -5,9 +5,6 @@ module DataCatalog
   class User
   
     include MongoMapper::Document
-    include Renderable
-
-    class InconsistentState < RuntimeError; end
 
     # == Attributes
 
@@ -19,47 +16,7 @@ module DataCatalog
     # == Indices
 
     ensure_index :email
-    ensure_index 'api_keys.api_key' # not tested
 
-    # == Associations
-
-    many :api_keys
-    many :ratings
-
-    # == Validations
-
-    # == Class Methods
-
-    def self.find_by_api_key(api_key)
-      find(:first, :conditions => { 'api_keys.api_key' => api_key })
-      # TODO: find :all and raise exception if more than 1 result
-    end
-
-    # == Various Instance Methods
-
-    def generate_api_key
-      salt = Config.environment_config["api_key_salt"]
-      s = "#{Time.now.to_f}#{salt}#{rand(100_000_000)}#{name}#{email}"
-      Digest::SHA1.hexdigest(s)
-    end
-
-    # Example usage:
-    #
-    #   user = User.new
-    #   user.add_api_key!({ :key_type => "primary" })
-    #   user.add_api_key!({ :key_type => "application" })
-    #   user.add_api_key!({ :key_type => "valet" })
-    #   user.save
-    #
-    def add_api_key!(params = {})
-      unless params[:api_key]
-        params.merge!({ :api_key => generate_api_key })
-      end
-      key = ApiKey.new(params)
-      self.api_keys << key
-      self.save!
-    end
-  
   end
 
 end
