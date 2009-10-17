a2153f8607225270e88ab96ea52a86e78957f1ff
David James <djames@sunlightfoundation.com>
2009-10-17 12:37:27 -0400

    working towards api_key

--- lib/builder.rb
+++ lib/builder.rb
@@ -12,30 +12,47 @@ module SinatraResource
       build_post
       build_put
       build_delete
+      build_helpers
     end
     
     def build_get_one
       @klass.get '/:id/?' do
+        check_permission(:read)
+        id = params.delete("id")
       end
     end
 
     def build_get_many
       @klass.get '/?' do
+        check_permission(:read)
       end
     end
     
     def build_post
       @klass.post '/?' do
+        check_permission(:create)
       end
     end
     
     def build_put
       @klass.put '/:id/?' do
+        check_permission(:update)
+        id = params.delete("id")
       end
     end
     
     def build_delete
       @klass.delete '/:id/?' do
+        check_permission(:delete)
+        id = params.delete("id")
+      end
+    end
+    
+    def build_helpers
+      @klass.helpers do
+        def check_permission(action)
+          # unauthorized! unless ...
+        end
       end
     end
     
