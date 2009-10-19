688bf0fba95624f5ca27ff8d4fb3669cde49b9e8
David James <djames@sunlightfoundation.com>
2009-10-19 14:27:45 -0400

    djsun-mongo_mapper 0.5.5.3

--- examples/datacatalog/config/config.rb
+++ examples/datacatalog/config/config.rb
@@ -8,7 +8,7 @@ module Config
   end
   
   def self.setup_mongomapper
-    gem 'djsun-mongo_mapper', '= 0.5.5.2'
+    gem 'djsun-mongo_mapper', '= 0.5.5.3'
     require 'mongo_mapper'
     MongoMapper.connection = new_mongo_connection
     MongoMapper.database = environment_config['mongo_database']
