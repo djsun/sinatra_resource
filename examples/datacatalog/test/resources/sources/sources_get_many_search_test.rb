29217d92f88cd7df36c059cb910e31d73196ab43
David James <djames@sunlightfoundation.com>
2009-10-30 07:54:44 -0400

    minor: fix class name of Sources search test

--- examples/datacatalog/test/resources/sources/sources_get_many_search_test.rb
+++ examples/datacatalog/test/resources/sources/sources_get_many_search_test.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../helpers/resource_test_helper')
 
-class SourcesGetManyResourceTest < ResourceTestCase
+class SourcesGetManySearchResourceTest < ResourceTestCase
 
   include DataCatalog
 
