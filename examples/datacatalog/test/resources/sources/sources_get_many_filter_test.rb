29e1d7c1aecd348d0fa500b90a61e310f1990b03
David James <djames@sunlightfoundation.com>
2009-11-05 11:48:35 -0500

    renaming *search_test to *filter_test

--- examples/datacatalog/test/resources/sources/sources_get_many_filter_test.rb
+++ examples/datacatalog/test/resources/sources/sources_get_many_filter_test.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../helpers/resource_test_helper')
 
-class SourcesGetManySearchResourceTest < ResourceTestCase
+class SourcesGetManyFilterResourceTest < ResourceTestCase
 
   include DataCatalog
 
--- examples/datacatalog/test/resources/sources_usages/sources_usages_get_many_filter_test.rb
+++ examples/datacatalog/test/resources/sources_usages/sources_usages_get_many_filter_test.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../helpers/resource_test_helper')
 
-class SourcesUsagesGetManySearchResourceTest < ResourceTestCase
+class SourcesUsagesGetManyFilterResourceTest < ResourceTestCase
 
   include DataCatalog
 
