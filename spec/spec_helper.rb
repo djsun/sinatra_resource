7472ebdb4cf62a2622c260baf97d0399d3affadd
David James <davidcjames@gmail.com>
2010-08-30 16:47:19 -0400

    Removing self-referring dependency

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'rubygems'
-require 'sinatra_resource'
 require 'spec'
 require 'spec/autorun'
 
